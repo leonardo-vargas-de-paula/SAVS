4215f833f734061f1871551e89320a7c118eead7
joao <rjoao.pedro05@gmail.com>
2024-10-11 18:34:07 -0400

    first commit

---
+++ src/main/java/com/example/sisapsoo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.sisapsoo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.sisapsoo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.sisapsoo to javafx.fxml;
+    exports com.example.sisapsoo;
+}
