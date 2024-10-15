3c371f95af1828ab89ad9110595f55d8ffc26260
joao <rjoao.pedro05@gmail.com>
2024-10-15 15:15:28 -0400

    feat: criar view de cadastro de funcionário

--- src/main/java/com/example/sisapsoo/Main.java
+++ src/main/java/com/example/sisapsoo/Main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 400);
-        stage.setTitle("Login");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("cadastro-funcionario-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+        stage.setTitle("Home");
         stage.setScene(scene);
         stage.show();
     }
